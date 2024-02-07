92320f2ba82652b850e18127ffa3e7c0e81d63e3
Laurentiu Popa <popa.laur.01@gmail.com>
2024-02-07 12:22:09 +0200

    Update BudgetTracking.tsx

--- client/src/components/Work/BudgetTracking.tsx
+++ client/src/components/Work/BudgetTracking.tsx
@@ -2,15 +2,15 @@
 export default function BudgetTracking() {
   return (
     <div className="h-[90%] flex gap-[2rem] flex-col">
-        <div className="bud-up ">
-            <p>Hello, James</p>
+      <div className="bud-up ">
+        <p>Hello, James</p>
+      </div>
+      <div className="bud-mid ">
+        <div className="bud-card">
+          <p>My balance</p>
+          <p>$26,129</p>
         </div>
-        <div className="bud-mid ">
-            <div className="bud-card">
-                <p>My balance</p>
-                <p>$26,129</p>
-            </div>
-        </div>
+      </div>
     </div>
   )
 }
